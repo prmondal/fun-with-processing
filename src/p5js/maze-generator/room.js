2e9d898a9c1286b198426499140cf0e6ac60ce74
Bumba <prasenjuetce@gmail.com>
2016-06-18 20:37:56 +0530

    random maze generation using random DFS

---
+++ src/p5js/maze-generator/room.js
@@ -0,0 +1,81 @@
+function Room(idx, center, width, height) {
+  this.idx = idx; // [i, j], i: row idx, j: col idx
+  
+  this.visited = false;
+  
+  this.walls = {
+    west: true,
+    south: true,
+    east: true,
+    north: true
+  };
+  
+  this.painted = false;
+  
+  this.center = center;
+  this.width = width;
+  this.height = height;
+  
+  this.corners = {
+    SW: createVector(this.center.x - this.width / 2, this.center.y + this.height / 2), 
+    SE: createVector(this.center.x + this.width / 2, this.center.y + this.height / 2),
+    NE: createVector(this.center.x + this.width / 2, this.center.y - this.height / 2),
+    NW: createVector(this.center.x - this.width / 2, this.center.y - this.height / 2)
+  }
+}
+
+//return unvisited neighbours
+//input- m: #grid rows, n: #grid cols, rooms: rooms grid array
+Room.prototype.neighbours = function(m, n, rooms) {
+  var neighbours = [];
+  var room;
+  
+  //west room
+  if(this.idx[1] - 1 >= 0) {
+    room = rooms[this.idx[0] * this.height + this.idx[1] - 1];
+    if(!room.visited) neighbours.push(room);
+  }
+  
+  //south room
+  if(this.idx[0] + 1 < m) {
+    room = rooms[(this.idx[0] + 1) * this.height + this.idx[1]];
+    if(!room.visited) neighbours.push(room);
+  }
+  
+  //east room
+  if(this.idx[1] + 1 < n) {
+    room = rooms[this.idx[0] * this.height + this.idx[1] + 1];
+    if(!room.visited) neighbours.push(room);
+  }
+  
+  //north room
+  if(this.idx[0] - 1 >= 0) {
+    room = rooms[(this.idx[0] - 1) * this.height + this.idx[1]];
+    if(!room.visited) neighbours.push(room);
+  }
+  
+  return neighbours;
+};
+
+Room.prototype.draw = function() {
+  //paint room
+  if(this.painted) {
+    stroke(0);
+    fill(128);
+    rect(this.corners.NW.x, this.corners.NW.y, this.width, this.height);
+  }
+  
+  stroke(255);
+  
+  //draw west wall
+  if(this.walls.west) line(this.corners.SW.x, this.corners.SW.y, this.corners.NW.x, this.corners.NW.y);
+  
+  //draw south wall
+  if(this.walls.south) line(this.corners.SW.x, this.corners.SW.y, this.corners.SE.x, this.corners.SE.y);
+  
+  //draw east wall
+  if(this.walls.east) line(this.corners.SE.x, this.corners.SE.y, this.corners.NE.x, this.corners.NE.y);
+  
+  //draw north wall
+  if(this.walls.north) line(this.corners.NW.x, this.corners.NW.y, this.corners.NE.x, this.corners.NE.y);
+};
---
+++ src/p5js/maze-generator/sketch.js
@@ -0,0 +1,81 @@
+var m = 20, 
+    n = 20, 
+    roomWidth = 20, 
+    roomHeight = 20, 
+    rooms = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  initGrid();
+  generateMaze();
+}
+
+function draw() {
+  background(0);
+  
+  drawRooms();
+}
+
+function initGrid() {
+  for(var i = 0; i < m; i++) {
+    for(var j = 0; j < n; j++) {
+      var center = createVector(roomWidth / 2 + j * roomWidth, roomHeight / 2 + i * roomHeight);
+      center.add(createVector((width - n * roomWidth) / 2, (height - m * roomHeight) / 2)); //place the grid at center of window
+      
+      rooms.push(new Room([i, j], center, roomWidth, roomHeight));
+    }
+  }
+}
+
+function drawRooms() {
+  rooms.forEach(function(room) {room.draw();});
+}
+
+function generateMaze() {
+  var stack = [];
+  var startRoomIdx = [Math.floor(random(0, m)), Math.floor(random(0, n))];
+  
+  //mark visited
+  var room = rooms[startRoomIdx[0] * roomHeight + startRoomIdx[1]];
+  room.visited = true;
+  
+  stack.push(room);
+  
+  //dfs with random strategy 
+  while(stack.length !== 0) {
+    var current = stack[stack.length - 1];
+    var neighbours = current.neighbours(m, n, rooms);
+    
+    if(neighbours.length === 0) {
+      stack.pop();
+      continue;
+    }
+    
+    //pick neighbour room randomly
+    var pickedRoom = neighbours[Math.floor(random(0, neighbours.length))];
+    
+    //break the wall
+    if(pickedRoom.idx[1] > current.idx[1]) {//east wall
+      current.walls.east = false;
+      pickedRoom.walls.west = false;
+    }
+    
+    if(pickedRoom.idx[1] < current.idx[1]) {//west wall
+      current.walls.west = false;
+      pickedRoom.walls.east = false;
+    }
+    
+    if(pickedRoom.idx[0] > current.idx[0]) {//south wall
+      current.walls.south = false;
+      pickedRoom.walls.north = false;
+    }
+    
+    if(pickedRoom.idx[0] < current.idx[0]) {//north wall
+      current.walls.north = false;
+      pickedRoom.walls.south = false;
+    }
+    
+    pickedRoom.visited = true;
+    stack.push(pickedRoom);
+  }
+}
