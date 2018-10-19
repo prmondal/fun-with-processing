2a0e033322b1216b9019dc986b0ad7e027b553b5
prmondal <prasenjuetce@gmail.com>
2018-10-19 19:38:59 +0530

    added sliders :)

--- src/p5js/spirograph/sketch.js
+++ src/p5js/spirograph/sketch.js
@@ -7,6 +7,9 @@ var l = 40;
 
 var noRotations = R / gcd(r, R);
 
+var RSlider, rSlider, lSlider, colorSlider;
+var sliderPosXOffset = w + 10, sliderPosYOffset = 40;
+
 function gcd(a, b) {
 	while(a != b) {
 		if(a > b) a = a - b;
@@ -18,12 +21,31 @@ function gcd(a, b) {
 
 function setup() {
 	createCanvas(w, h);
-    noLoop();
+	colorMode(HSB);
+
+	RSlider = createSlider(1, w/2, 105);
+	RSlider.position(sliderPosXOffset, sliderPosYOffset);
+	
+	rSlider = createSlider(1, R, 75);
+	rSlider.position(sliderPosXOffset, 2 * sliderPosYOffset);
+	
+	lSlider = createSlider(0, w, 40);
+	lSlider.position(sliderPosXOffset, 3 * sliderPosYOffset);
+
+	colorSlider = createSlider(0, 255, 255);
+	colorSlider.position(sliderPosXOffset, 4 * sliderPosYOffset);
 }
 
 function draw() {
 	background(0);
+
 	var deltaAngle = PI/1800;
+
+	R = RSlider.value();
+	r = rSlider.value();
+	l = lSlider.value();
+
+	//TODO: noRotations = R / gcd(r, R);
 
 	for(var t = 0; t <= noRotations * TWO_PI; t += deltaAngle) {
 		var a = R - r;
@@ -32,7 +54,7 @@ function draw() {
 		var x = a * Math.cos(t) + l * Math.cos(b);
 		var y = a * Math.sin(t) - l * Math.sin(b);
 		
-		stroke(0,0,255);
+		stroke(colorSlider.value(),255,255);
 		point(x+w/2, y+h/2);
 	}
 }
